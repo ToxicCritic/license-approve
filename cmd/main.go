2fadab4e52479c13177ff7d58a52ecabb42c5815
ToxicCritic <Em0zzIoN@yandex.ru>
2025-01-15 12:46:30 +0300

    Добавить методы пользовательского шифрования/дешифрования

--- cmd/main.go
+++ cmd/main.go
@@ -3,30 +3,24 @@ package main
 import (
 	"LicenseApp/pkg/db"
 	"LicenseApp/pkg/handlers"
-	"LicenseApp/pkg/security"
 	"log"
 	"net/http"
 )
 
 func main() {
-	// 1. Загрузка ключей
-	err := security.LoadKeys("config/keys/private_key.pem", "config/keys/public_key.pem")
-	if err != nil {
-		log.Fatal("Error loading keys:", err)
-	}
-
-	// 2. Инициализация базы
 	db.Init()
 	db.Migrate()
 
-	// 3. Маршруты
 	http.HandleFunc("/license-requests", handlers.GetLicenseRequestsHandler)
 	http.HandleFunc("/approve-license", handlers.ApproveLicenseRequestHandler)
 	http.HandleFunc("/reject-license", handlers.RejectLicenseRequestHandler)
 
-	log.Println("Server started on port 8080...")
-	err = http.ListenAndServe(":8080", nil)
+	certFile := "config/certs/server.crt"
+	keyFile := "config/certs/server.key"
+
+	log.Println("HTTPS server started on port 8443...")
+	err := http.ListenAndServeTLS(":8443", certFile, keyFile, nil)
 	if err != nil {
-		log.Fatal("Error starting server:", err)
+		log.Fatal("Error starting HTTPS server:", err)
 	}
 }
--- pkg/models/licence.go
+++ pkg/models/licence.go
@@ -4,9 +4,10 @@ import "time"
 
 // License представляет саму лицензию
 type License struct {
-	ID         int       `json:"id"`
-	UserID     int       `json:"user_id"`
-	LicenseKey string    `json:"license_key"`
-	Status     string    `json:"status"` // Статус лицензии (active, expired)
-	IssuedAt   time.Time `json:"issued_at"`
+	ID               int       `json:"id"`
+	UserID           int       `json:"user_id"`
+	LicenseKey       string    `json:"license_key"`
+	LicenseSignature string    `json:"license_signature"`
+	Status           string    `json:"status"` // active, expired, approvedJ
+	IssuedAt         time.Time `json:"issued_at"`
 }
